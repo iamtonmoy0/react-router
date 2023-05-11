d3b07dc4379c02b04e3a1c03f2e0f5c3e2651526
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-11 14:58:16 -0400

    feat: basic route added without element

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"/about",
+    element: <div>this is about</div>
+  },
+{
+    path:"/contact",
+    element: <div>this is contact</div>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
