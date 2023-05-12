e826ea2fb1e24bcef652a95eaba83b65dd01a21f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-12 11:58:52 -0400

    feat: load data based on dynamic route functionality added

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from "react-router-dom";
 
 export const FriendDetail = () => {
+	const friend =useLoaderData();
+	const {name,username,email,phone}=friend;
+	const{street,city,zipcode}=friend.address;
+	
 	return (
 		<div>
-			dta
-		</div>
+			<h3>Name: {name}</h3>
+			<h4>User Name : {username} </h4>
+			<p>Email: {email} </p>
+			<p>Phone : {phone}</p>
+			<p>Location: {street},{city},{zipcode} </p>
+					</div>
 	);
 }
 
