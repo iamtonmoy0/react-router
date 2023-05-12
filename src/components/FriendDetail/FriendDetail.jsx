b8e466085c90a34932121d0d3e1e0f98a468fdcd
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-12 19:05:34 -0400

    feat: backward navigation routing added

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,10 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 export const FriendDetail = () => {
 	const friend =useLoaderData();
 	const {name,username,email,phone}=friend;
 	const{street,city,zipcode}=friend.address;
-	
+	const navigate=useNavigate()
+	const handleBack=()=>{
+		navigate(`/friends`)
+	}
 	return (
 		<div>
 			<h3>Name: {name}</h3>
@@ -12,6 +15,7 @@ export const FriendDetail = () => {
 			<p>Email: {email} </p>
 			<p>Phone : {phone}</p>
 			<p>Location: {street},{city},{zipcode} </p>
+			<button onClick={handleBack}>Back</button>
 					</div>
 	);
 }
