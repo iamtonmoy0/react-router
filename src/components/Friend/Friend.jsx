481fa3ddbe14768be3eb99cfb3c56a4c322cb793
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-12 18:42:28 -0400

    feat: useNavigate hook used for navigation

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import './Friend.css'
 export const Friend = (friend) => {
 		const {name,id,email,phone}=friend.friend;
+		const navigate = useNavigate()
+		const handleNavigation=()=>{
+			navigate(`/friend/${id}`)
+		}
 	return (
 		<div className="friendCart">
 			<h3>Name:{name} </h3>
 			<h4>Email : {email}</h4>
 			<p>Number :{phone} </p>
-			<Link to={`/friend/${id}`}>Show more</Link>
+			<button onClick={handleNavigation}>Show more</button>
 		</div>
 	);
 }
